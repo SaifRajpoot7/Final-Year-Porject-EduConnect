f17055697dda1a33bc5254e5efdb969c93e92f12
Saif Rajpoot <saif.itdeveloper@gmail.com>
2026-01-19 23:35:50 +0500

    origin fix

--- server/app.js
+++ server/app.js
@@ -29,8 +29,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(cors({
-  origin: ["https://final-year-porject-edu-connect.vercel.app", allowedOrigins],
-  methods: ["POST", "GET", "PUT", "DELETE"],
+  origin: ["https://final-year-porject-edu-connect.vercel.app", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  
   credentials: true
 }));
 
